b4a25d545c3b4b02550e7af73da1a80a29417938
Alessia Tosi <al.to@live.com>
2020-06-08 15:42:35 +0100

    added function to calculate polarity of sentences using TextBlob

--- src/analyse_text.py
+++ src/analyse_text.py
@@ -1,13 +1,14 @@
 import numpy as np
 from typing import List
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from textblob import TextBlob
 analyser = SentimentIntensityAnalyzer()
 
 
 def get_sentiment_score_VDR(text: List[str],
                             score_type: str = "compound") -> List[float]:
     """
-    Calculate nltk Vader sentiment analysis score (score_type: 'compound' default, 'pos', 'neg')
+    Calculates nltk Vader sentiment analysis score (score_type: 'compound' default, 'pos', 'neg')
     for each sentence in a paragraph text. The input must be a list of string sentences.
 
     Return a list of scores (as float), one score for each sentence in the paragraph text.
@@ -19,22 +20,45 @@ def get_sentiment_score_VDR(text: List[str],
         score_type :    'compound' (default), 'pos' or 'neg'
 
     Returns:
-        A list of sentiment scores. E.g., [0.0, 0.1]
+        A list of sentiment scores in the [-1.0, 1.0] range.
     """
 
     try:
-        scores = (np.nan if len(text) == 0 else
-                  [analyser.polarity_scores(s)[score_type] for s in text])
+        scores = [
+            np.nan if len(s) == 0 else analyser.polarity_scores(s)[score_type]
+            for s in text
+        ]
         return scores
 
     except TypeError as e:
         return e
 
 
+def get_sentiment_score_TB(text: str) -> float:
+    """
+    Calculates sentiment analysis score using TextBlob
+    for the provided text.
+
+    If text is empty, return NaN.
+
+    Args:
+        text :          a paragraph of text as string.
+
+    Returns:
+        A sentiment scores in the [-1.0, 1.0] range.
+    """
+
+    score = np.nan if len(text) == 0 else TextBlob(text).sentiment.polarity
+
+    return score
+
+
 if __name__ == "__main__":
 
     test_text = [
-        "I love running.", "But oh how much I hate stretching afterwards!"
+        "I love running.", "But oh how much I hate stretching afterwards!", ""
     ]
 
     print(get_sentiment_score_VDR(test_text))
+
+    print([get_sentiment_score_TB(text) for text in test_text])
