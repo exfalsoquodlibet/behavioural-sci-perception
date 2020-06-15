b0ecc401bfa4cc2986ffb391150cf62e72286d24
Alessia Tosi <al.to@live.com>
2020-06-15 18:38:31 +0100

    added function to load YAML files

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,4 @@
+import yaml
 from typing import Callable
 from functools import reduce
 
@@ -29,3 +30,38 @@ def chain_functions(*f_args: Callable) -> Callable:
         A function that applies the chained functions.
     """
     return reduce(chain_2funcs, f_args, lambda x: x)
+
+
+def load_config_yaml(config_file: str) -> dict:
+    """Load YAML configuration file at path config_file
+
+    Args:
+        config_file (str): path to the yaml file
+
+    Returns:
+        the configuration object as a dictionary
+
+    """
+
+    try:
+        with open(config_file, "r") as inp:
+            config = yaml.load(inp, Loader=yaml.FullLoader)
+        print(config_file, "has been successfully loaded as a dict")
+        return config
+    except FileNotFoundError as e:
+        raise type(e)(config_file, "not found!")
+    except IOError as e:
+        raise type(e)("Couldn't open", config_file)
+
+
+if __name__ == "__main__":
+
+    import os
+    # Load configuration file
+    CONFIG_FILE_NAME = "config_news_data.yaml"
+    DIR_EXT = os.environ.get("DIR_EXT")
+    CONFIG_FILE = os.path.join(DIR_EXT, CONFIG_FILE_NAME)
+
+    print(CONFIG_FILE)
+
+    CONFIG = load_config_yaml(CONFIG_FILE)
