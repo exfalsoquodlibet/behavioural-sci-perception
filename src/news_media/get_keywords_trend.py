8c2d4db99b0c53b23b852f45a0bdbb81f3d877a1
Alessia Tosi <al.to@live.com>
2020-06-25 09:04:23 +0100

    added functions to collect the 3-sentence opinion context for each keyword occurrence in any article

--- src/news_media/get_keywords_trend.py
+++ src/news_media/get_keywords_trend.py
@@ -9,12 +9,15 @@
 
 """
 
+import re
 import os
 import pandas as pd
+from typing import List, Union
 from datetime import datetime
 from sklearn.feature_extraction.text import CountVectorizer
 
 from src.utils import load_config_yaml
+from src.preproc_text import tokenise_sent
 
 # Constants
 UK_FILENAME = "uk_news.csv"
@@ -112,6 +115,204 @@ class NewsArticles:
             lambda x: (x > 0).sum()).reset_index(name='count')
 
 
+def collect_kword_opinioncontext(
+        article: str) -> List[List[Union[str, tuple]]]:
+    """
+    Extracts the opinion context for each keyword' occurrence in a article.
+
+    The opinion context is defined as the sentence where the keyword occurrence and the
+    following two sentences.
+
+    Args:
+        article:    text of the article as string.
+
+    Returns:
+        A list of list. Each sublist contains:
+        - first element: the keyword (str) that occurred in the article
+        - second element: a tuple of (index i of first sentence where the keyword occurs, the first senetence)
+        - third element: a tuple of (index i+1, text) of the next sentence
+        - fourth element: a tuple of (index i+2, text) of the next-next sentence
+
+    """
+
+    # add a space after dots or commas that have no space afterwards
+    # (e.g., 'I know.But' -> 'I know. But')
+    article = re.sub(r'(?<=[.;!?:])(?=[^\s])', r' ', article)
+
+    sentences = tokenise_sent(article.lower())
+
+    # max_idx = len(sentences)
+    results = []
+    for keyw in VOCAB:
+        for idx, sentence in enumerate(sentences):
+            if keyw in sentence:
+                context1 = (idx, sentence)
+                kw_result = [context1]
+                try:
+                    context2 = (idx + 1, sentences[idx + 1])
+                    kw_result.append(context2)
+                    context3 = (idx + 2, sentences[idx + 2])
+                    kw_result.append(context3)
+                except IndexError:
+                    pass
+                kw_result = [keyw, kw_result]
+                results.append(kw_result)
+
+    return remove_duplicates(results)
+
+
+# TODO: streamline / refactor
+def remove_duplicates(
+        list_results: List[list]) -> List[Union[str, List[tuple]]]:
+    """
+    Removes duplicate results.
+
+    Example: cases that are double counted because both "nudge theory" and "nudge"
+    have been identified for the same sentence.
+
+    Args:
+        list_results:   List of results. Each result is also a list whose first element is the keyword (str)
+                        and second element is a list of tuples; the first element of the first tuple is the index of the sentence
+                        where the keyword appears.
+
+    Returns:
+        De-duplicated list of results.
+    """
+
+    # extract list of keywords
+    # and  # extract index of first sentence for each keyword
+    kws_idx_first_sent_dict = {
+        result[0]: result[1][0][0]
+        for result in list_results
+    }
+    kws_list = [result[0] for result in list_results]
+
+    # keyword list contains both 'nudge theory' and 'nudge' for same sentences
+    if ("nudge theory" and "nudge"
+            in kws_list) and (kws_idx_first_sent_dict.get('nudge')
+                              == kws_idx_first_sent_dict.get('nudge theory')):
+        # get rid of "nudge" results as it is a duplicate
+        list_results = [
+            result for result in list_results if result[0] != "nudge"
+        ]
+
+    # keyword list contains both 'nudge uniit' and 'nudge' for same sentences
+    if ("nudge unit" and "nudge"
+            in kws_list) and (kws_idx_first_sent_dict.get('nudge')
+                              == kws_idx_first_sent_dict.get('nudge unit')):
+        # get rid of "nudge" results as it is a duplicate
+        list_results = [
+            result for result in list_results if result[0] != "nudge"
+        ]
+
+    # keyword list contains both 'nudges' and 'nudge' for same sentences
+    if ("nudges" and "nudge"
+            in kws_list) and (kws_idx_first_sent_dict.get('nudge')
+                              == kws_idx_first_sent_dict.get('nudges')):
+        # get rid of "nudge" results as it is a duplicate
+        list_results = [
+            result for result in list_results if result[0] != "nudge"
+        ]
+
+    # keyword list contains both 'choice architecture' and 'choice architect' for same sentences
+    if ("choice architecture" and "choice architect" in kws_list) and (
+            kws_idx_first_sent_dict.get('choice architect')
+            == kws_idx_first_sent_dict.get('choice architecture')):
+        # get rid of "choice architect" results as it is a duplicate
+        list_results = [
+            result for result in list_results
+            if result[0] != "choice architect"
+        ]
+
+    if ("behavioural insights" and "behavioural insight" in kws_list) and (
+            kws_idx_first_sent_dict.get('behavioural insight')
+            == kws_idx_first_sent_dict.get('behavioural insights')):
+        list_results = [
+            result for result in list_results
+            if result[0] != "behavioural insight"
+        ]
+
+    #
+    if ("behavioural insights team" and "behavioural insights" in kws_list
+        ) and (kws_idx_first_sent_dict.get('behavioural insights')
+               == kws_idx_first_sent_dict.get('behavioural insights team')):
+        list_results = [
+            result for result in list_results
+            if result[0] != "behavioural insights"
+        ]
+
+    if ("behavioural insights team" and "behavioural insight" in kws_list
+        ) and (kws_idx_first_sent_dict.get('behavioural insight')
+               == kws_idx_first_sent_dict.get('behavioural insights team')):
+        list_results = [
+            result for result in list_results
+            if result[0] != "behavioural insight"
+        ]
+
+    #
+    if ("behavioural scientists" and "behavioural scientist" in kws_list) and (
+            kws_idx_first_sent_dict.get('behavioural scientist')
+            == kws_idx_first_sent_dict.get('behavioural scientists')):
+        list_results = [
+            result for result in list_results
+            if result[0] != "behavioural scientist"
+        ]
+
+    if ("behavioural sciences" and "behavioural science" in kws_list) and (
+            kws_idx_first_sent_dict.get('behavioural science')
+            == kws_idx_first_sent_dict.get('behavioural sciences')):
+        list_results = [
+            result for result in list_results
+            if result[0] != "behavioural science"
+        ]
+
+    if ("behavioural economists" and "behavioural economist" in kws_list) and (
+            kws_idx_first_sent_dict.get('behavioural economist')
+            == kws_idx_first_sent_dict.get('behavioural economists')):
+        list_results = [
+            result for result in list_results
+            if result[0] != "behavioural economist"
+        ]
+
+    if ("behavioural analysts" and "behavioural analyst" in kws_list) and (
+            kws_idx_first_sent_dict.get('behavioural analyst')
+            == kws_idx_first_sent_dict.get('behavioural analysts')):
+        list_results = [
+            result for result in list_results
+            if result[0] != "behavioural analyst"
+        ]
+
+    if ("psychologists" and "psychologist"
+            in kws_list) and (kws_idx_first_sent_dict.get('psychologist')
+                              == kws_idx_first_sent_dict.get('psychologists')):
+        list_results = [
+            result for result in list_results if result[0] != "psychologist"
+        ]
+
+    if ("coronavirus" and "corona"
+            in kws_list) and (kws_idx_first_sent_dict.get('corona')
+                              == kws_idx_first_sent_dict.get('coronavirus')):
+        list_results = [
+            result for result in list_results if result[0] != "corona"
+        ]
+
+    if ("covid19" and "covid"
+            in kws_list) and (kws_idx_first_sent_dict.get('covid')
+                              == kws_idx_first_sent_dict.get('covid19')):
+        list_results = [
+            result for result in list_results if result[0] != "covid"
+        ]
+
+    if ("covid-19" and "covid"
+            in kws_list) and (kws_idx_first_sent_dict.get('covid')
+                              == kws_idx_first_sent_dict.get('covid-19')):
+        list_results = [
+            result for result in list_results if result[0] != "covid"
+        ]
+
+    return list_results
+
+
 if __name__ == "__main__":
 
     uk_news = NewsArticles(country="uk")
@@ -176,5 +377,3 @@ if __name__ == "__main__":
         os.path.join(DIR_DATA_INT, "articles_with_keyword_count_by_date.csv"))
     counts_by_kwgroups_date.to_csv(
         os.path.join(DIR_DATA_INT, "kwgroups_count_by_date.csv"))
-
-    # plot
