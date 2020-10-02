8ee82a739817ed241439b68e027d8a922ddb7025
Alessia Tosi <al.to@live.com>
2020-10-02 09:25:50 +0100

    procedure to calculate frequency measures changed in get_keyword_trend.py
    now input datafile is the data files coded for sentiments by researchers

--- src/news_media/get_keywords_trend.py
+++ src/news_media/get_keywords_trend.py
@@ -1,16 +1,18 @@
 """
-# lower case
-# sentence tokenise
-# work tokenise
-# COUNT: extract number of occurrences of <KEY-WORD> in each article [(key_word, count)]
-# BINARY: extract whether <KEY-WORD> occurred in article or not [(key_word, 1 or 0)]
-# encode date as date
-# plot trend over time
+This script produces the keywords frequency measures to use to analyse the prominence of keywords in the press
+over time.
 
+Input data: TO ADD
+
+Outputs: TO ADD
+
+How to run it to calculate frequency measure for a two-week (fornight) period:
+python -m src.news_media.get_keyword_trend -unit_agg "2W-MON"
 """
 
 import re
 import os
+import pickle
 import pandas as pd
 
 from typing import List
@@ -25,9 +27,7 @@ from src.utils import load_config_yaml, chain_functions
 from src.preproc_text import tokenise_sent, tokenise_word, remove_punctuation, remove_stopwords, flatten_irregular_listoflists
 
 # Constants
-UK_FILENAME1 = "uk_news.csv"
-UK_FILENAME2 = "uk_news_sample2.csv"
-USA_FILENAME = "us_news.csv"
+UK_FILENAME = "preproc_kword_sent.pickle"
 CONFIG_FILE_NAME = "keywords.yaml"
 SUBWK_TO_KW_MAP = "subkw_to_kw_map.yaml"
 DIR_DATA_INT = os.environ.get("DIR_DATA_INTERIM")
@@ -42,6 +42,14 @@ KWORDS = CONFIG['Actors'] + CONFIG['BehavSci'] + CONFIG['Behav_ins'] + CONFIG[
         'Psych'] + CONFIG['Econ_behav'] + CONFIG['Econ_irrational'] + CONFIG[
             'Nudge'] + CONFIG['Nudge_choice'] + CONFIG['Nudge_pater']
 OTHER_IMPORTANT_WORDS = CONFIG['Covid'] + CONFIG['Fatigue'] + CONFIG['Immunity']
+
+# as some sub-keywords are quoted
+QUOTED_KWORDS = ["'" + word + "'" for word in KWORDS] + [
+    "'" + word for word in KWORDS
+] + [word + "'" for word in KWORDS] + ['"' + word + '"' for word in KWORDS] + [
+    '"' + word for word in KWORDS
+] + [word + '"' for word in KWORDS]
+ALL_WORDS_TO_EXTRACT = KWORDS + QUOTED_KWORDS
 
 # keywords that are composed by more than one word
 NONUNIGRAMS = [
@@ -68,7 +76,7 @@ def from_ngrams_to_unigrams(text: str) -> str:
 def remove_special_symbols(text: str) -> str:
     symbols = [
         "©", "\xad", "•", "…", "●", "“", "”", "•'", "\u200b", "£", "'", "'s",
-        "com/"
+        "·", "»", "com/"
     ]
     for symb in symbols:
         if symb not in text:
@@ -104,54 +112,62 @@ class NewsArticles:
 
     __COLS_GROUPBY_DICT = {}
 
-    def __init__(self, country: str = "uk"):
+    def __init__(self):
 
-        if country == "usa":
-            csv_filepath = os.path.join(DIR_DATA_INT, USA_FILENAME)
-            df = pd.read_csv(csv_filepath)
-        else:
-            csv_filepath1 = os.path.join(DIR_DATA_INT, UK_FILENAME1)
-            csv_filepath2 = os.path.join(DIR_DATA_INT, UK_FILENAME2)
-            df1 = pd.read_csv(csv_filepath1)
-            df2 = pd.read_csv(csv_filepath2)
-            # combine the two
-            df = df1.append(df2)
+        with open(os.path.join(DIR_DATA_INT, UK_FILENAME), "rb") as input_file:
+            df = pickle.load(input_file)
 
-        # Remove rows that mark start of batches
-        bool_series = df["title"].str.startswith("Title (", na=False)
-        df = df[~bool_series].copy()
-        # Remove duplicates
-        df.drop_duplicates(subset="title", keep='last', inplace=True)
-        # Remove cases where there is no article text (are float not str)
-        df = df[[isinstance(text, str) for text in df.full_text]]
-        # Proprocess Text
-        df['preproc_text'] = [
-            TEXT_PREPROC_PIPE(article) for article in df.full_text
+        # ENSURE DATES BEFORE 27-JAN-2020 WERE REMOVED
+        # this exclude 6 articles published on 23-JAN and 26-JAN 2020
+        print(df.shape)
+        df = df[~df.pub_date.isin(
+            ['January 23, 2020 Thursday', 'January 26, 2020 Sunday'])].copy()
+        print(df.shape)
+        # this version of data has repeasted measure per article (1 row per keyword occurrence)
+
+        # create version of data with only unique info to each article: id, pub_date_dt, title, full_text
+        df_uniq_article = df.drop_duplicates('article_id', 'last')[[
+            'article_id', 'pub_date_dt', 'title', 'full_text'
+        ]]
+
+        # Preprocess Text
+        df_uniq_article['preproc_text'] = [
+            TEXT_PREPROC_PIPE(article) for article in df_uniq_article.full_text
+        ]
+        # Calculate count of unique nouns per article ('word_count')
+        df_uniq_article['word_count'] = [
+            NewsArticles.get_num_ngrams(text, 1)
+            for text in df_uniq_article.preproc_text
         ]
 
-        self.data = df
-        self.country = country
+        # Append 'word_count' to doc-term matrix dataset as well
+        df = df.merge(df_uniq_article[['article_id', 'word_count']],
+                      on='article_id')
+
+        self.data_raw = df
+        self.data_uniq_article = df_uniq_article
         self.__COLS_GROUPBY_DICT = expand_dict(SUBKEYWORDS_TO_KEYWORDS_MAP)
-        self.dates = NewsArticles._extract_date(dates_series=df.pub_date)
         # private class-instance attributes
         self._allwords_raw_tf = NewsArticles._compute_allwords_raw_tf(
-            news_df=df)
-        self._unigram_count_perdoc = [
-            NewsArticles.get_num_ngrams(text, 1) for text in df.preproc_text
-        ]
-        self._subkword_raw_tf = None
-        self._kword_rawfreq = None
-        self.kword_rawfreq_week = None
-        self._kword_relfreq_week = None
+            news_df=df_uniq_article)
+        # NOT NEEDED self._unigram_count_perdoc = [
+        #    NewsArticles.get_num_ngrams(text, 1)
+        #    for text in df_uniq_article.preproc_text
+        # ]
+        self._kword_rawfreq = NewsArticles.get_kword_rawfreq(news_df=df)
+
+        self.kword_rawfreq_agg = None
+        self._kword_relfreq_agg = None
         self._kword_yn_occurrence = None
-        self.kword_docfreq_week = None
-        self._kword_reldocfreq_week = None
-        self._kword_rfrdf_week = None
+        self.kword_docfreq_agg = None
+        self._kword_reldocfreq_agg = None
+        self._kword_rfrdf_agg = None
 
     @staticmethod
-    def _compute_allwords_raw_tf(news_df: pd.DataFrame) -> pd.DataFrame:
+    def _compute_allwords_raw_tf(news_df: pd.DataFrame,
+                                 text_col='preproc_text') -> pd.DataFrame:
         """
-        Computes the document-term frequency matrix for all the unigram words in the preprocessing texts.
+        Computes the document-term frequency matrix for all the unigram nouns in the preprocessing texts.
 
         Args:
             news_df: pandas.Dataframe, results of `get_news_articles.IngestNews`.
@@ -164,7 +180,7 @@ class NewsArticles:
                               ngram_range=(1, 1),
                               token_pattern=r"(?u)\b\w\w+\b")
 
-        results_mat = vec.fit_transform(news_df['preproc_text'])
+        results_mat = vec.fit_transform(news_df[text_col])
 
         # sparse to dense matrix
         results_mat = results_mat.toarray()
@@ -177,14 +193,13 @@ class NewsArticles:
 
         # add article id and pub date as indexes
         # we use the property of CountVectorizer to keep the order of the original texts
-        out_df["pub_date"] = NewsArticles._extract_date(
-            dates_series=news_df["pub_date"])
-        out_df["word_count"] = [
-            NewsArticles.get_num_ngrams(text=article, ngram=1)
-            for article in news_df['preproc_text']
-        ]
-        out_df.set_index(['pub_date', 'word_count'], append=True, inplace=True)
-        out_df.rename_axis(["id", "pub_date", "word_count"], inplace=True)
+        out_df["pub_date"] = news_df["pub_date_dt"].tolist()
+        out_df["article_id"] = news_df["article_id"].tolist()
+        out_df["word_count"] = news_df["word_count"].tolist()
+
+        out_df.set_index(['pub_date', 'article_id', 'word_count'],
+                         append=True,
+                         inplace=True)
 
         return out_df
 
@@ -192,34 +207,45 @@ class NewsArticles:
     def allwords_raw_tf(self):
         return self._allwords_raw_tf
 
-    @property
-    def subkword_raw_tf(self):
+    @staticmethod
+    def get_kword_rawfreq(news_df: pd.DataFrame) -> pd.DataFrame:
         """
-        Returns the raw frequency count of the sub-keywords per articles.
-        """
-        if self._subkword_raw_tf is None:
-            self._subkword_raw_tf = self._allwords_raw_tf[[
-                col for col in self._allwords_raw_tf.columns if col in KWORDS
-            ]]
+        Calculates the raw frequency count of the keywords per articles.
 
-        return self._subkword_raw_tf
+        CountVectorizer was missing to detect a few cases (16 out of more than 1600 cases)
+        probably because some of these keyword instances were quoted or not recognise as 'noun'
+        in the pre-processing step.
+
+        To ensure consistency with number of cases of keyword occurrences in the sentiment-analysis,
+        we will use the keyword occurrences extracted for the keyword-sentiment extraction,
+        after they have been coded and filtered by researchers. Researchers also deleted a few
+        identified cases while manually coding the sentences, as they were duplicates. 
+        """
+
+        df_list_kwords = news_df.groupby(
+            ['article_id', 'word_count',
+             'pub_date_dt'])['kword'].apply(list).reset_index()
+
+        df_kwords = df_list_kwords.join(
+            pd.get_dummies(
+                pd.DataFrame(
+                    df_list_kwords['kword'].tolist()).stack()).sum(level=0))
+
+        # drop 'kword' col containing list of keywords now that each keyword is a separate col
+        df_kwords.drop('kword', axis=1, inplace=True)
+
+        # set indices
+        df_kwords.set_index(['pub_date_dt', 'article_id', 'word_count'],
+                            append=True,
+                            inplace=True)
+
+        return df_kwords
 
     @property
     def kword_rawfreq(self):
-        """
-        Calculates and returns the raw frequency count of each higher-level keyword per article.
-        """
-        if self._kword_rawfreq is None:
-            try:
-                self._kword_rawfreq = self._subkword_raw_tf.groupby(
-                    self.__COLS_GROUPBY_DICT, axis=1).sum()
-            except AttributeError:
-                raise AttributeError(
-                    "`subkword_raw_tf` must be calculated first!")
-
         return self._kword_rawfreq
 
-    def get_kword_rawfreq_week(self, unit="W-MON"):
+    def get_kword_rawfreq_agg(self, unit="W-MON"):
         """
         Calculates and returns the raw keyword frequency count of each higher-level keyword per week or forthnight.
         A week starts on a Monday (i.e., dates are grouped by week starting the first Monday before the date),
@@ -239,30 +265,30 @@ class NewsArticles:
             raise KeyError("'unit' must be either 'W-MON' or '2W-MON'")
 
         try:
-            self.kword_rawfreq_week = self._kword_rawfreq.reset_index(
+            self.kword_rawfreq_agg = self._kword_rawfreq.reset_index(
                 'word_count').resample(unit,
                                        level=1,
                                        label='left',
                                        closed='left').sum()
-            self.kword_rawfreq_week.index.names = [f"{unit_name}_starting"]
+            self.kword_rawfreq_agg.index.names = [f"{unit_name}_starting"]
         except AttributeError:
             raise AttributeError("`kword_rawfreq` must be calculated first!")
-        return self.kword_rawfreq_week
+        return self.kword_rawfreq_agg
 
     @property
-    def kword_relfreq_week(self):
+    def kword_relfreq_agg(self):
         """Calculates the weekly relative keyword frequency by dividing the number of a keyword's occurrences in a week
         by the total number of words published that week."""
-        if self._kword_relfreq_week is None:
+        if self._kword_relfreq_agg is None:
             try:
-                self._kword_relfreq_week = self.kword_rawfreq_week.iloc[:, 1:].div(
-                    self.kword_rawfreq_week.word_count, axis=0)
+                self._kword_relfreq_agg = self.kword_rawfreq_agg.iloc[:, 1:].div(
+                    self.kword_rawfreq_agg.word_count, axis=0)
             except AttributeError:
                 raise AttributeError(
-                    "`kword_rawfreq_week` must be calculated first!")
-        return self._kword_relfreq_week
+                    "`kword_rawfreq_agg` must be calculated first!")
+        return self._kword_relfreq_agg
 
-    def get_kword_docfreq_week(self, unit="W-MON"):
+    def get_kword_docfreq_agg(self, unit="W-MON"):
         """
         Calculates document-frequency (df) for each keyword and week or fortnight. For each keyword, k:
         docfreq_k = [{d in D | k in d}] / |D|
@@ -281,54 +307,55 @@ class NewsArticles:
         else:
             raise KeyError("'unit' must be either 'W-MON' or '2W-MON'")
 
-        if self.kword_docfreq_week is None:
+        if self.kword_docfreq_agg is None:
             try:
                 articles_count = self._kword_yn_occurrence.reset_index(
-                    'id').id.resample(unit,
-                                      level=0,
-                                      label='left',
-                                      closed='left').count()
+                    'article_id').article_id.resample(unit,
+                                                      level=1,
+                                                      label='left',
+                                                      closed='left').count()
                 kword_doc_freqs = self._kword_yn_occurrence.resample(
                     unit, level=1, label='left', closed='left').sum()
-                self.kword_docfreq_week = kword_doc_freqs.merge(articles_count,
-                                                                on='pub_date')
-                self.kword_docfreq_week.rename(columns={'id': 'article_count'},
-                                               inplace=True)
-                self.kword_docfreq_week.index.names = [f"{unit_name}_starting"]
+                self.kword_docfreq_agg = kword_doc_freqs.merge(
+                    articles_count, on='pub_date_dt')
+                self.kword_docfreq_agg.rename(
+                    columns={'article_id': 'article_count'}, inplace=True)
+                self.kword_docfreq_agg.index.names = [f"{unit_name}_starting"]
             except AttributeError:
                 raise AttributeError(
-                    "`kword_rawfreq` must be calculated first!")
-        return self.kword_docfreq_week
+                    "`kword_yn_occurrence` must be calculated first!")
+        return self.kword_docfreq_agg
 
     @property
-    def kword_reldocfreq_week(self):
+    def kword_reldocfreq_agg(self):
         """Calculates and returns the keyword's relative document frequency for each week.
         This is the number of articles published that week in which the keyword is mentioned,
         divided by the total number of articles published that week."""
 
-        if self._kword_reldocfreq_week is None:
+        if self._kword_reldocfreq_agg is None:
             try:
-                self._kword_reldocfreq_week = self.kword_docfreq_week.iloc[:, :-1].div(
-                    self.kword_docfreq_week.article_count, axis=0)
+                # iloc[:, :-1] excludes the 'article_count' col
+                self._kword_reldocfreq_agg = self.kword_docfreq_agg.iloc[:, :-1].div(
+                    self.kword_docfreq_agg.article_count, axis=0)
             except AttributeError:
                 raise AttributeError(
-                    "`kword_docfreq_week` must be calculated first!")
-        return self._kword_reldocfreq_week
+                    "`kword_docfreq_agg` must be calculated first!")
+        return self._kword_reldocfreq_agg
 
     @property
-    def kword_rfrdf_week(self):
+    def kword_rfrdf_agg(self):
         """"""
-        if self._kword_rfrdf_week is None:
+        if self._kword_rfrdf_agg is None:
             try:
-                unit_name = self._kword_relfreq_week.index.name
+                unit_name = self._kword_relfreq_agg.index.name
                 print(unit_name)
                 long_kword_relfreq = pd.melt(
-                    self._kword_relfreq_week.reset_index(),
+                    self._kword_relfreq_agg.reset_index(),
                     id_vars=[unit_name],
                     var_name='kword',
                     value_name='rkf')
                 long_kword_reldocfreq = pd.melt(
-                    self._kword_reldocfreq_week.reset_index(),
+                    self._kword_reldocfreq_agg.reset_index(),
                     id_vars=[unit_name],
                     var_name='kword',
                     value_name='rdf')
@@ -337,15 +364,15 @@ class NewsArticles:
                 # calculate final metrics
                 week_rkf_rdf[
                     "rkf*rdf"] = week_rkf_rdf['rkf'] * week_rkf_rdf['rdf']
-                self._kword_rfrdf_week = week_rkf_rdf[[
-                    unit_name, 'kword', 'rkf*rdf'
+                self._kword_rfrdf_agg = week_rkf_rdf[[
+                    unit_name, 'kword', 'rkf*rdf', 'rkf', 'rdf'
                 ]]
 
             except AttributeError:
                 raise AttributeError(
-                    "'kword_relfreq_week' and 'kword_reldocfreq_week' must both be calculated first!"
+                    "'kword_relfreq_agg' and 'kword_reldocfreq_agg' must both be calculated first!"
                 )
-        return self._kword_rfrdf_week
+        return self._kword_rfrdf_agg
 
     @property
     def kword_yn_occurrence(self):
@@ -446,29 +473,27 @@ if __name__ == "__main__":
 
     print(f"Aggregation method selected: {unit_agg}")
 
-    uk_news = NewsArticles(country="uk")
-    print(uk_news.data.shape)
+    uk_news = NewsArticles()
+    print(uk_news.data_raw.shape)
+    print(uk_news.data_uniq_article.shape)
 
     # calculate all the metrics
-    uk_news.subkword_raw_tf
     uk_news.kword_rawfreq
-    uk_news.get_kword_rawfreq_week(unit=unit_agg)
-    uk_news.kword_relfreq_week
+    uk_news.get_kword_rawfreq_agg(unit=unit_agg)
+    uk_news.kword_relfreq_agg
     uk_news.kword_yn_occurrence
-    uk_news.get_kword_docfreq_week(unit=unit_agg)
-    uk_news.kword_reldocfreq_week
-    uk_news.kword_rfrdf_week
+    uk_news.get_kword_docfreq_agg(unit=unit_agg)
+    uk_news.kword_reldocfreq_agg
+    uk_news.kword_rfrdf_agg
 
     # save them as csv so that they can be loaded in in R
     uk_news.kword_rawfreq.to_csv(
         os.path.join(DIR_DATA_INT, "kword_rawfreq.csv"))
     uk_news.kword_yn_occurrence.to_csv(
         os.path.join(DIR_DATA_INT, "kword_yn_occurrence.csv"))
-    uk_news.kword_rawfreq_week.to_csv(
+    uk_news.kword_rawfreq_agg.to_csv(
         os.path.join(DIR_DATA_INT, f"kword_rawfreq_{unit_agg}.csv"))
-    uk_news.kword_docfreq_week.to_csv(
+    uk_news.kword_docfreq_agg.to_csv(
         os.path.join(DIR_DATA_INT, f"kword_docfreq_{unit_agg}.csv"))
-    uk_news.kword_reldocfreq_week.to_csv(
-        os.path.join(DIR_DATA_INT, f"kword_reldocfreq_{unit_agg}.csv"))
-    uk_news.kword_rfrdf_week.to_csv(
+    uk_news.kword_rfrdf_agg.to_csv(
         os.path.join(DIR_DATA_INT, f"kword_rfrdf_{unit_agg}.csv"))
